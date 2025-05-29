037ce7b70a7cc22ce4254f1995d1efd78ad762db
michael b. jordan <aisideveloper@gmail.com>
2025-05-29 09:49:51 +0800

    added searchbar to main.jsx

--- invoice-management-tool/src/pages/main/Main.jsx
+++ invoice-management-tool/src/pages/main/Main.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../../components/navbar/Navbar";
+import Searchbar from "../../components/searchbar/Searchbar";
 
 export default function Main() {
   return (
     <header>
       <Navbar />
+      <Searchbar />
     </header>
   );
 }
