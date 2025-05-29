daefa6ab78f4f77478262a86df3cac96955c125b
michael b. jordan <aisideveloper@gmail.com>
2025-05-29 09:27:08 +0800

    add navbar to main.jsx

--- invoice-management-tool/src/pages/main/Main.jsx
+++ invoice-management-tool/src/pages/main/Main.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
-import { invoke } from "@tauri-apps/api/core";
+import Navbar from "../../components/navbar/Navbar";
 
 export default function Main() {
-  const navigate = useNavigate();
-  const { logout } = useAuth();
-
-  const handleLogout = () => {
-    logout();
-  };
-
   return (
-    <main>
-      <p>LOGOUT NOW</p>
-      <button onClick={handleLogout}>Logout</button>
-    </main>
+    <header>
+      <Navbar />
+    </header>
   );
 }
