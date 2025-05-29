01a681e7eee8fb200fed34b5cfee10bae2d42e95
michael b. jordan <aisideveloper@gmail.com>
2025-05-29 11:19:34 +0800

    updated searchbar

--- invoice-management-tool/src/components/searchbar/Searchbar.jsx
+++ invoice-management-tool/src/components/searchbar/Searchbar.jsx
@@ -2,8 +2,9 @@ import "./Searchbar.css";
 
 export default function Searchbar() {
   return (
-    <div>
-      <p>im search bar</p>
+    <div className="searchbarcontainertest">
+      <p>search icon goes here</p>
+      <p className="searchbartest">im search bar</p>
     </div>
   );
 }
