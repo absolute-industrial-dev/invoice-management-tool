fa1df5617900a97e90976b8b8ee7418625fe9e35
michael b. jordan <aisideveloper@gmail.com>
2025-05-29 11:19:49 +0800

    added skeleton searchby

---
+++ invoice-management-tool/src/components/searchby/Searchby.jsx
@@ -0,0 +1,9 @@
+import "./Searchby.css";
+
+export default function Searchby() {
+  return (
+    <div className="searchbycontainertest">
+      <p className="searchbytest">im search by</p>
+    </div>
+  );
+}
