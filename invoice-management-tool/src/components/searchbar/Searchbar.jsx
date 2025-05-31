02ff670b984e900907c514a2fca4d5d443af6b61
michael b. jordan <aisideveloper@gmail.com>
2025-05-31 14:43:45 +0800

    added searchbar functionality

--- invoice-management-tool/src/components/searchbar/Searchbar.jsx
+++ invoice-management-tool/src/components/searchbar/Searchbar.jsx
@@ -2,14 +2,20 @@ import ParticleBackground from "../particles/Particles";
 import "./Searchbar.css";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
-export default function Searchbar({ className = '' }) {
+export default function Searchbar({
+  className = "",
+  searchQuery,
+  setSearchQuery,
+}) {
   return (
     <div className={`searchbar ${className}`}>
-      <ParticleBackground/>
+      <ParticleBackground />
       <MagnifyingGlassIcon className="search-icon" />
       <input
         type="text"
         placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
     </div>
   );
