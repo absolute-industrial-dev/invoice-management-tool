15f1901125c7de67bf81023a40849354531b675c
infotech <infotech.aisi@gmail.com>
2025-06-02 10:56:57 +0800

    refactor: created export to excel button

---
+++ invoice-management-tool/src/components/export-to-excel/ExportToExcel.jsx
@@ -0,0 +1,9 @@
+import "./ExportToExcel.css"
+import { RiFileExcel2Fill } from "react-icons/ri";
+
+export default function ExportToExcel () {
+
+    return <button className="ete-button">
+       <RiFileExcel2Fill />
+    </button>
+}
