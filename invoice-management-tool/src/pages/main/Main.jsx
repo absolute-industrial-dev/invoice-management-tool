25b7a5ee9d1c4c5f320aa3c17b45ec332fd43fe0
michael b. jordan <aisideveloper@gmail.com>
2025-06-02 10:05:58 +0800

    style: removed trailing whitespaces

--- invoice-management-tool/src/pages/main/Main.jsx
+++ invoice-management-tool/src/pages/main/Main.jsx
@@ -1,12 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
 import Table from "../../components/table/Table";
 import ThemeToggle from "../../components/themetoggle/Theme";
-import "./Main.css" ;
+import "./Main.css";
 
 export default function Main() {
   return (
-    <div className="table-container"> 
-      <ThemeToggle className="mode"/>
+    <div className="table-container">
+      <ThemeToggle className="mode" />
       <header>
         <Navbar />
       </header>
