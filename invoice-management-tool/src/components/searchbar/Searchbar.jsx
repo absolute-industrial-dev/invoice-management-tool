a1999806909749cba100fe139f01ea9eff6ea8eb
MINGW64 <aisideveloper@gmail.com>
2025-06-03 14:19:40 +0800

    feat: added search query clear on escape

--- invoice-management-tool/src/components/searchbar/Searchbar.jsx
+++ invoice-management-tool/src/components/searchbar/Searchbar.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import ParticleBackground from "../particles/Particles";
 import "./Searchbar.css";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
@@ -7,6 +8,17 @@ export default function Searchbar({
   searchQuery,
   setSearchQuery,
 }) {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        setSearchQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  });
+
   return (
     <div className={`searchbar ${className}`}>
       <ParticleBackground />
