bafaa0f067c2c0d669f27ba87e25ad96d933d35d
Martha Luther King Jr. IV <aisiindustrial@gmail.com>
2025-06-03 16:40:02 +0800

    feat: implement toast notifications on app

--- invoice-management-tool/src/App.jsx
+++ invoice-management-tool/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/main/Main";
 import Login from "./pages/login/Login";
 import { useAuth } from "./hooks/useAuth";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import { useEffect } from "react";
 import { monitorDueDate } from "./lib/invoiceService";
@@ -20,9 +21,12 @@ export default function App() {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <Routes>
+    <>
+      <Routes>
       <Route path="/" element={user ? <Navigate to="/main" /> : <Login />} />
       <Route path="/main" element={user ? <Main /> : <Navigate to="/" />} />
     </Routes>
+    <Toaster position="bottom-right"/>
+    </>
   );
 }
