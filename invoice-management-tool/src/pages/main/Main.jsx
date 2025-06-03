360bd1c653e6bb71130f9037bb4a5bb5d9080925
angelicalynf <angelicalynf@gmail.com>
2025-06-03 11:44:15 +0800

    style: minor css fixes

--- invoice-management-tool/src/pages/main/Main.jsx
+++ invoice-management-tool/src/pages/main/Main.jsx
@@ -1,12 +1,10 @@
 import Navbar from "../../components/navbar/Navbar";
 import Table from "../../components/table/Table";
-import ThemeToggle from "../../components/themetoggle/Theme";
 import "./Main.css";
 
 export default function Main() {
   return (
     <div className="table-container">
-      <ThemeToggle className="mode" />
       <header>
         <Navbar />
       </header>
