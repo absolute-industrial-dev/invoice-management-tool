72c32741c49925f2a123a7b0319fcdb72cac130e
infotech <infotech.aisi@gmail.com>
2025-06-24 14:39:36 +0800

    feat: added delete modal with functionality

---
+++ invoice-management-tool/src/components/deletemodal/DeleteModal.jsx
@@ -0,0 +1,51 @@
+import toast from "react-hot-toast";
+import { deleteInvoice } from "../../lib/invoiceService";
+import "./DeleteModal.css";
+export default function DeleteModal({
+  isOpen,
+  setIsOpen,
+  invoice,
+  setSelectedInvoice,
+  reloadInvoices,
+}) {
+  if (!isOpen) return null;
+  const invoiceId = invoice.id;
+
+  const handleClose = () => {
+    setSelectedInvoice(null);
+    setIsOpen(false);
+  };
+
+  const handleDelete = async () => {
+    const response = await deleteInvoice(invoiceId);
+
+    if (!response) {
+      toast.error(`Error deleting invoice: ${response.message}`);
+    }
+
+    toast.success("Deleted invoice successfully.");
+    reloadInvoices();
+  };
+
+  return (
+    <div
+      className="modal-overlay"
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="modal-title"
+      onClick={handleClose}
+    >
+      <div className="modal-container">
+        <span>
+          This will delete all existing records for this invoice. Are you sure
+          you want to delete?
+        </span>
+
+        <div className="modal-buttons">
+          <button onClick={handleDelete}>Delete</button>
+          <button onClick={handleClose}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
+}
