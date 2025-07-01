27df3a20b9b085a0a313ae92821ba02b34168d12
seb <infotech.aisi@gmail.com>
2025-07-01 14:51:31 +0800

    feat: added mass convert modal with functionality

---
+++ invoice-management-tool/src/components/mass-convert-modal/MassConvertModal.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import "./MassConvertModal.css";
+import { massConvert } from "../../lib/invoiceService";
+import toast from "react-hot-toast";
+const statuses = ["Sales Log", "Collectibles", "Last File"];
+
+export default function MassConvertModal({ isOpen, setIsOpen }) {
+  if (!isOpen) return null;
+
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [initialStatus, setInitialStatus] = useState("Sales Log");
+  const [finalStatus, setFinalStatus] = useState("Collectibles");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleMassConvert = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const response = await massConvert(
+      startDate,
+      endDate,
+      initialStatus,
+      finalStatus
+    );
+    if (!response.success) {
+      setIsLoading(false);
+      return toast.error(`${response.error}`);
+    }
+
+    setIsLoading(false);
+    return toast.success(`Updated ${response.count} invoices successfully.`);
+  };
+
+  return (
+    <div
+      className="modal-overlay"
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="massconvert-modal-title"
+      onClick={handleClose}
+    >
+      <form
+        className="massconvert-modal-container"
+        onClick={(e) => e.stopPropagation()}
+        onSubmit={(e) => handleMassConvert(e)}
+      >
+        <label>Select invoice dates to convert</label>
+        <div className="date-range-container">
+          <input
+            required
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          ></input>
+          <span> To </span>
+          <input
+            required
+            type="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          ></input>
+        </div>
+
+        <div className="status-range-container">
+          <label>From</label>
+          <select onChange={(e) => setInitialStatus(e.target.value)}>
+            {statuses.map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+
+          <label>To</label>
+          <select onChange={(e) => setFinalStatus(e.target.value)}>
+            {statuses
+              .filter((status) => status !== initialStatus)
+              .map((status) => (
+                <option key={status} value={status}>
+                  {status}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="massconvert-modal-buttons">
+          {" "}
+          <button type="submit" disabled={isLoading}>
+            Convert
+          </button>
+          <button type="button" disabled={isLoading}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
